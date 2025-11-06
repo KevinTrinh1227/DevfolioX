d3a8d9834d86982d274f748cd457d3e7b5355e39
KevinTrinh1227 <kevintrinh1227@gmail.com>
2025-11-06 00:45:19 -0600

    Added analytics for vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { siteConfig } from "../config/siteConfig";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   title: siteConfig.name + " – " + siteConfig.title,
@@ -14,6 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Analytics />
       <body className="min-h-screen bg-background text-foreground antialiased">
         {children}
       </body>
